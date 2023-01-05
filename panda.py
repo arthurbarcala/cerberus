98e1b8f2a9ab08281a1d4fd4ad8974f73184f82a
arthurbarcala <arthurfbarcalars@gmail.com>
2023-01-05 11:54:09 -0300

    fazendo uns coisinhos

--- panda.py
+++ panda.py
@@ -8,8 +8,13 @@ data_xlsx = pd.read_excel('pokemon_data.xlsx')
 data_txt = pd.read_csv('pokemon_data.txt', delimiter='\t')
 
 #imprimindo dados 
-print(data.head(3)) 
+""" print(data.head(3)) 
 
 print(data.columns)
 
-print(data['Name'])
+print(data['Name']) """
+
+print(f"Quantidade de colunas: {len(data_xlsx.columns)}")
+#for x in data_xlsx.columns:
+    #print(f"{x} = {data_xlsx.columns[0].index(x)}")
+print(data_xlsx.column)
